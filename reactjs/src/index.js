56bfe1ca0ccf29a4a5b17661163674f5450ba358
limdongsun0814 <asme12@naver.com>
2024-01-20 16:35:07 +0900

    흠 조금더 봐야겠다

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -4,10 +4,13 @@ import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import Test from "./test/Test";
+import HooksTest from "./hooks/HooksTest";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <>
+    <HooksTest></HooksTest>
+    <hr></hr>
     <Test />
   </>
 );
