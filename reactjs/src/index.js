8a6198b512c200759e8a162fc44928d27cefc97d
wldnjs127 <149056497+wldnjs127@users.noreply.github.com>
2024-01-21 11:29:28 +0900

    jyeoni_Feat

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -14,7 +14,7 @@ root.render(
     <Test />
   </>
 );
-
+//져니 브랜치 이동완!
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
