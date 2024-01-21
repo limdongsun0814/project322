6d46427affef16fc655a3b8f8a1cc81ff16d56cb
yena <iyena0915@gmail.com>
2024-01-21 11:29:05 +0900

    충돌테스트라요

--- reactjs/src/index.js
+++ reactjs/src/index.js
@@ -14,7 +14,7 @@ root.render(
     <Test />
   </>
 );
-
+//충돌테스트yn
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
